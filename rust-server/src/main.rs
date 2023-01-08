6feb0a76a83c3d1ccd34f0cb359d6892aa6d59c3
Karama Timur <timurkarama@gmail.com>
2023-01-08 17:45:00 +0400

    main func

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!();
+use api_user::*;
+pub mod api_user;
+
+fn main() -> Result<(), std::io::Error>
+{
+    let f = async {
+        let data = DataBase
+        {
+            users: HashMap::new(),
+            u_max_id: 0,
+            groups: HashMap::new(),
+            g_max_id: 0,
+            u_gs: HashMap::new(),
+        };
+        let state = Arc::new(Mutex::new(data));
+        let mut app = tide::with_state(state);
+
+
+        app.listen("127.0.0.1:8080").await
+    };
+    futures::executor::block_on(f)
 }

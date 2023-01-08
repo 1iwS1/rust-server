f8a5c6e7bacf96ab2b8b1c0262439132fe24be64
iwS <msp1108@mail.ru>
2023-01-08 17:20:54 +0300

    functions added for api

--- rust-server/src/api_user.rs
+++ rust-server/src/api_user.rs
@@ -41,3 +41,48 @@ pub struct DataBase
     pub g_max_id: Id,
     pub u_gs: HashMap<UGId, UGProps>,
 }
+
+pub fn get_field<T>(object: &Map<String, Value>, key: &str) -> T
+    where
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    object.get(key).unwrap().as_str().unwrap().parse().unwrap()
+}
+
+pub fn resp_data(value: Value) -> Response
+{
+    Response::builder(200)
+        .body(tide::Body::from_json(&value).unwrap())
+        .build()
+}
+
+pub fn resp_empty() -> Response
+{
+    Response::builder(200).build()
+}
+
+pub fn resp_error(msg: &str) -> Response
+{
+    Response::builder(400)
+        .body(tide::Body::from_json(&json!({"error": msg})).unwrap())
+        .build()
+}
+
+pub fn new_user(input_obj: &Map<String, Value>, state: &Arc<Mutex<DataBase>>) -> Response
+{
+    let name: String = get_field(input_obj, "name");
+    if name.len() > 0
+    {
+        let mut guard = state.lock().unwrap();
+        let id = guard.u_max_id;
+        guard.users.insert(id, name);
+        guard.u_max_id += 1;
+
+        resp_data(json!({"id": id}))
+    }
+    else
+    {
+        resp_error("bad name")
+    }
+}
